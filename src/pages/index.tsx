cf4e8bb220479524c0fea5b3f42849b11ed7de92
pa large <pierreantoine.large@gmail.com>
2023-10-27 12:58:47 +0200

    first commit - debugging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Hero from '@/components/presentation'
-import NavBar from '@/components/navbar'
+import Hero from '../components/presentation'
+import NavBar from '../components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
